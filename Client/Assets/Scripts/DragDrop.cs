52a1b13b1ebeb5c1b46e2592b2ec3a13ffba51ba
Mark Diehr <mdiehr@gmail.com>
2014-08-11 22:53:18 -0400

    Pawn, DragDrop script, chessboard background

---
+++ Client/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,40 @@
+﻿// DragDrop.cs
+// Allows dragging and dropping an item on the grid
+
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour {
+	
+	public float _gridOffset = 0.5f;
+	public float _gridScale = 1.0f;
+
+	private Vector3 _mousePoint;
+	private Vector3 _touchPoint;
+	private Vector3 _delta;
+
+	void OnMouseDown() {
+		_mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		_touchPoint = _mousePoint;
+	}
+
+	void OnMouseDrag() {
+		_mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		_delta = _mousePoint - _touchPoint;
+		transform.Translate(_delta);
+		// Save the new touch point
+		_touchPoint = _mousePoint;
+	}
+
+	void OnMouseUp() {
+		transform.localPosition = GetClosestSnapPoint();
+	}
+
+	private Vector3 GetClosestSnapPoint() {
+		Vector3 position = transform.localPosition;
+		float x = position.x;
+		float y = position.y;
+		x = Mathf.Floor(x/_gridScale) + _gridOffset;
+		y = Mathf.Floor(y/_gridScale) + _gridOffset;
+		return new Vector3(x, y, 0f);
+	}
+}
