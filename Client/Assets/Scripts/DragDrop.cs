540c8f2f62fe777727af7eda04669e547f01f5ee
Mark Diehr <mdiehr@gmail.com>
2014-08-11 23:19:26 -0400

    Pickup/Drop tweens

--- Client/Assets/Scripts/DragDrop.cs
+++ Client/Assets/Scripts/DragDrop.cs
@@ -8,30 +8,53 @@ public class DragDrop : MonoBehaviour {
 	public float _gridOffset = 0.5f;
 	public float _gridScale = 1.0f;
 	public float _snapTime = 0.2f;
+	public float _holdScale = 1.25f;
 
 	private Vector3 _mousePoint;
 	private Vector3 _touchPoint;
 	private Vector3 _delta;
 
+	GoTweenConfig _tweenUp;
+	GoTweenConfig _tweenDown;
+
+	void Awake() {
+		_tweenUp = new GoTweenConfig().setEaseType( GoEaseType.CubicOut ).scale(this.transform.localScale * _holdScale);
+		_tweenDown = new GoTweenConfig().setEaseType( GoEaseType.CubicOut ).scale(this.transform.localScale);
+	}
+
+	// Grab the mouse position
 	void OnMouseDown() {
-		_mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		_mousePoint = CalcMousePoint();
+		// Save the touch point
 		_touchPoint = _mousePoint;
+		Go.to(transform, _snapTime, _tweenUp);
 	}
 
+	// Translate the object based on how the mouse moved
 	void OnMouseDrag() {
-		_mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		_delta = _mousePoint - _touchPoint;
-		transform.Translate(_delta);
-		// Save the new touch point
+		_mousePoint = CalcMousePoint();
+		// Move by the difference
+		transform.Translate(_mousePoint - _touchPoint);
+		// Save the touch point
 		_touchPoint = _mousePoint;
 	}
 
+	// Drop the item into the grid
 	void OnMouseUp() {
-		// Tween to the position
-		var config = new GoTweenConfig().setEaseType( GoEaseType.ExpoOut ).position( GetClosestSnapPoint(), false );
-		Go.to(transform, _snapTime, config);
+		// Tween to the closest tile position
+		GoTweenConfig tweenConfig = new GoTweenConfig()
+			.setEaseType( GoEaseType.ExpoOut )
+			.position(GetClosestSnapPoint(), false);
+		Go.to(transform, _snapTime, tweenConfig);
+		Go.to(transform, _snapTime, _tweenDown);
 	}
 
+	// Returns the mouse position in world coordinates
+	private Vector3 CalcMousePoint() {
+		return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	// Returns a vector that is snapped to the nearest point on the grid
 	private Vector3 GetClosestSnapPoint() {
 		Vector3 position = transform.localPosition;
 		float x = position.x;
